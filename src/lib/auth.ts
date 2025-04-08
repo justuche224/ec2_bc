095a6734214b8f11bafcdc6ed0ca68e535236bf8
justuche224 <justuche224@gmail.com>
2025-04-08 08:50:42 +0100

    gsf

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -43,6 +43,14 @@ export const auth = betterAuth({
       },
     },
   },
+  advanced: {
+    defaultCookieAttributes: {
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",  // Allows CORS-based cookie sharing across subdomains
+      partitioned: true, // New browser standards will mandate this for foreign cookies
+  },
+  },
   trustedOrigins: process.env.NODE_ENV === "production" ? [process.env.FRONTEND_URL] : ["http://localhost:5173", "http://localhost:3000", "https://ecohavest.org",],
   logger: {
     level: "debug",
